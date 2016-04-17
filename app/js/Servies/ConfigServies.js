d73336a99cc6ab25ed51250faf3fb4e8db960bd8
JeyKey <j-k.92@mail.ru>
2016-04-17 17:08:24 +0600

    111

--- app/js/Servies/ConfigServies.js
+++ app/js/Servies/ConfigServies.js
@@ -4,8 +4,8 @@ var AppConfig = angular.module('AppConfig', ['ngResource']);
 
     AppConfig.factory('Config', ['$resource', '$rootScope', function($resource, $rootScope){
 
-      var TplUrl = "http://localhost/tpl/img/category";
-      var UrlValue = "http://localhost/api/v1/";
+      var TplUrl = "http://192.168.0.101/tpl/";
+      var UrlValue = "http://192.168.0.101/api/v1/";
       var TokenValue = "a3ca5cf04464775a3ca0e1d2944d5f43875b2507";
 
       $rootScope.currency = "тг.";
